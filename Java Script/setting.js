426d5c4ed3d50aef283ed09789c0d46d5d65bc4f
Umer SHami <batch22.cs.018@gmail.com>
2024-07-02 18:37:21 +0500

    Initial commit

--- Java Script/setting.js
+++ Java Script/setting.js
@@ -1,16 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('settingsBtn').addEventListener('click', function() {
-        document.getElementById('settingsBar').style.display = 'block';
-    });
-
-    document.getElementById('closeBtn').addEventListener('click', function() {
-        document.getElementById('settingsBar').style.display = 'none';
-    });
-
-    // Optional: Close the settings bar if the user clicks outside of it
-    window.addEventListener('click', function(event) {
-        if (event.target == document.getElementById('settingsBar')) {
-            document.getElementById('settingsBar').style.display = 'none';
-        }
-    });
+document.getElementById('settingsBtn').addEventListener('click', function() {
+    var st = document.getElementById('ps');
+    if (st.style.display === 'none' || st.style.display === '') {
+        st.style.display = 'block';
+    } else {
+        st.style.display = 'none';
+    }
 });

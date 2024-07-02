2b0443cd526f7d4be94d37325da14a166a1067bc
Umer SHami <batch22.cs.018@gmail.com>
2024-07-02 19:05:21 +0500

    Initial commit

--- Java Script/setting.js
+++ Java Script/setting.js
@@ -1,3 +1,15 @@
+
+document.addEventListener('click', function(event) {
+    var settingsBtn = document.getElementById('settingsBtn');
+    var settingsDropdown = document.getElementById('ps');
+
+    // Check if the click is outside the settings dropdown
+    if (event.target !== settingsBtn && !settingsDropdown.contains(event.target)) {
+        settingsDropdown.style.display = 'none';
+    }
+});
+
+
 document.getElementById('settingsBtn').addEventListener('click', function() {
     var st = document.getElementById('ps');
     if (st.style.display === 'none' || st.style.display === '') {
